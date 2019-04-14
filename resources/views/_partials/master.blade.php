285715c155423e7b51b036abee805c880a404be9
Eduard Robu <robu.edi@gmail.com>
2019-04-14 15:42:12 +0100

    cache booster

--- resources/views/_partials/master.blade.php
+++ resources/views/_partials/master.blade.php
@@ -27,7 +27,7 @@
         <meta name="theme-color" content="#e62c33">
 
         <link rel="icon" href="/assets/img/logo.ico">
-        <link rel="stylesheet" type="text/css" href="/assets/css/app.min.css?v=4">
+        <link rel="stylesheet" type="text/css" href="/assets/css/app.min.css?v=5">
 
         @yield('stylesheets')
 
