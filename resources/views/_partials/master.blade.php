1927dee2dd20593c0ec5ee6418e6941440c37716
Eduard Robu <robu.edi@gmail.com>
2019-05-09 01:18:10 +0100

    course description

--- resources/views/_partials/master.blade.php
+++ resources/views/_partials/master.blade.php
@@ -27,7 +27,7 @@
         <link rel="canonical" href="{{ url()->current() }}" />
 
         <link rel="icon" href="/assets/img/logo.ico">
-        <link rel="stylesheet" type="text/css" href="/assets/css/bundle.min.css?v=10">
+        <link rel="stylesheet" type="text/css" href="/assets/css/bundle.min.css?v=11">
 
         @yield('stylesheets')
 
