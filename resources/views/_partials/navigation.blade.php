9ac9d41b2bce125798f56242b30b5ec10504b6e4
Eduard Robu <robu.edi@gmail.com>
2019-05-18 15:47:05 +0100

    facebook added

--- resources/views/_partials/navigation.blade.php
+++ resources/views/_partials/navigation.blade.php
@@ -16,7 +16,8 @@
                 <li><a href="/user/sign-in" class="@if(url()->current() === url('/user/sign-in')) active @endif" >Sign In / Register</a></li>
             @endif
             <li class="display-inline-block" ><a href="https://twitter.com/_redtutorial" target="_blank" rel="noopener" class="twitter-account" ><i class="fab fa-twitter"></i></a></li>
-            <li class="display-inline-block" ><a href="https://www.youtube.com/channel/UCdiASKn5toBvQZV_TJKCM8A" target="_blank" rel="noopener" class="twitter-account" ><i class="fab fa-youtube"></i></a></li>
+            <li class="display-inline-block" ><a href="https://www.youtube.com/channel/UCdiASKn5toBvQZV_TJKCM8A" target="_blank" rel="noopener" ><i class="fab fa-youtube"></i></a></li>
+            <li class="display-inline-block" ><a href="https://www.facebook.com/com.RedTutorial" target="_blank" rel="noopener" ><i class="fab fa-facebook-square"></i></a></li>
         </ul>
     </div>
 
