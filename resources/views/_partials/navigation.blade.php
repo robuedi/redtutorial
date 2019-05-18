c0fa12701a8f9c936aa5deaad152cedf963bfcff
Eduard Robu <robu.edi@gmail.com>
2019-05-18 14:40:23 +0100

    youtube channel added

--- resources/views/_partials/navigation.blade.php
+++ resources/views/_partials/navigation.blade.php
@@ -8,7 +8,6 @@
         <div class="trigger-sidebar" ></div>
 
         <ul class="navigation-links">
-            {{--<li ><a href="#"  >My Account</a></li>--}}
             <li><a href="/" class="@if(url()->current() === url('/')) active @endif" >Tutorials</a></li>
             <li><a href="/contact-us" class="@if(url()->current() === url('/contact-us')) active @endif" >Contact Us</a></li>
             @if(\Cartalyst\Sentinel\Laravel\Facades\Sentinel::check())
@@ -17,8 +16,8 @@
                 <li><a href="/user/sign-in" class="@if(url()->current() === url('/user/sign-in')) active @endif" >Sign In / Register</a></li>
             @endif
             <li><a href="https://twitter.com/_redtutorial" target="_blank" rel="noopener" class="twitter-account" ><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.youtube.com/channel/UCdiASKn5toBvQZV_TJKCM8A" target="_blank" rel="noopener" class="twitter-account" ><i class="fab fa-youtube"></i></a></li>
         </ul>
     </div>
-
 
 </nav>
