8412771fcc13cd8345196e753c55061be1bf2223
Eduard Robu <robu.edi@gmail.com>
2019-06-01 15:12:37 +0100

    style updates

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'RED Tutorial'),
+    'name' => env('APP_NAME', 'RedTutorial'),
 
     /*
     |--------------------------------------------------------------------------
