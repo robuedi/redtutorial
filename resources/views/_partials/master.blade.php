7efb46373f350145309921c28c477875929c892d
Eduard Robu <robu.edi@gmail.com>
2019-07-18 17:59:45 +0100

    style updates

--- resources/views/_partials/master.blade.php
+++ resources/views/_partials/master.blade.php
@@ -31,7 +31,7 @@
         @endif
 
         <link rel="icon" href="/assets/img/logo.ico">
-        <link rel="stylesheet" type="text/css" href="/assets/css/bundle.min.css?v=2">
+        <link rel="stylesheet" type="text/css" href="/assets/css/bundle.min.css?v=3">
 
         @yield('stylesheets')
 
