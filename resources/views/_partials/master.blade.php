ccb30784e7762a93e219aa168e40154c68a8a111
Eduard Robu <robu.edi@gmail.com>
2019-08-09 23:30:15 +0100

    small style update

--- resources/views/_partials/master.blade.php
+++ resources/views/_partials/master.blade.php
@@ -31,7 +31,7 @@
         @endif
 
         <link rel="icon" href="/assets/img/logo.ico">
-        <link rel="stylesheet" type="text/css" href="/assets/css/bundle.min.css?v=17">
+        <link rel="stylesheet" type="text/css" href="/assets/css/bundle.min.css?v=18">
 
         @yield('stylesheets')
 
