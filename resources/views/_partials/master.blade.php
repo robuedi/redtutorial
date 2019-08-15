842341456a62ec668ce1dfea384b6a18e3089d0c
Eduard Robu <robu.edi@gmail.com>
2019-08-15 14:36:29 +0100

    cache booster

--- resources/views/_partials/master.blade.php
+++ resources/views/_partials/master.blade.php
@@ -33,8 +33,7 @@
         @endif
 
         <link rel="icon" href="/assets/img/logo_new.ico">
-        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900|Roboto:400,700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="/assets/css/bundle.min.css?v=40">
+        <link rel="stylesheet" type="text/css" href="/assets/css/bundle.min.css?v=41">
 
         @yield('stylesheets')
 
